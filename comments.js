1379b7090afd802a7119cc65eb918f2dd880e11c
YacoobDev <131663945+YacoobDev@users.noreply.github.com>
2023-07-27 16:54:46 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,92 +1,16 @@
 //create a web server
-const express = require('express');
-const app = express();
-//use the express router
-const router = express.Router();
-//import the comments model
-const Comments = require('../models/comments');
-//import the body parser
-const bodyParser = require('body-parser');
-//use the body parser
-app.use(bodyParser.urlencoded({extended: true}));
-//use the json parser
-app.use(bodyParser.json());
+//create a web server
+import { Router } from 'express';
+const router = Router();
 
-//get all comments
-router.get('/', (req, res) => {
-    Comments.findAll()
-        .then((comments) => {
-            res.send(comments);
-        })
-        .catch((err) => {
-            res.send("error: " + err);
-        });
-});
+//import the controller
+import { create, destroy } from '../controllers/comments_controller';
 
-//get comment by id
-router.get('/:id', (req, res) => {
-    Comments.findByPk(req.params.id)
-        .then((comments) => {
-            if (comments) {
-                res.send(comments);
-            } else {
-                res.send("Comment does not exist");
-            }
-        })
-        .catch((err) => {
-            res.send("error: " + err);
-        });
-});
+//create a route for create
+router.post('/create',create);
 
-//add a comment
-router.post('/', (req, res) => {
-    if (!req.body.comment) {
-        res.status(400);
-        res.json({
-            error: "Bad Data"
-        });
-    } else {
-        Comments.create(req.body)
-            .then(() => {
-                res.send("Comment Added");
-            })
-            .catch((err) => {
-                res.send("Error: " + err);
-            });
-    }
-});
-
-//update a comment
-router.put('/:id', (req, res) => {
-    if (!req.body.comment) {
-        res.status(400);
-        res.json({
-            error: "Bad Data"
-        });
-    } else {
-        Comments.update(
-            {comment: req.body.comment},
-            {where: {id: req.params.id}}
-        )
-            .then(() => {
-                res.send("Comment Updated");
-            })
-            .error((err) => res.send(err));
-    }
-});
-
-//delete a comment
-router.delete('/:id', (req, res) => {
-    Comments.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(() => {
-            res.send("Comment Deleted!");
-        })
-        .error((err) => res.send(err));
-});
+//create a route for delete
+router.get('/destroy/:id',destroy);
 
 //export the router
-module.exports = router;
+export default router;
