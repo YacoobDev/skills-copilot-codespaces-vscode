cdd8571deb3a88652cb32d40dd41a116c76a2498
YacoobDev <131663945+YacoobDev@users.noreply.github.com>
2023-07-27 17:07:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,8 +2,6 @@
 import { Router } from 'express';
 const router = Router();
 
-//import the comments model
-import Comments, { find } from '../models/comments';
 
 //GET request for all comments
 router.get('/', (req, res) => {
@@ -15,35 +13,6 @@ router.get('/', (req, res) => {
         .catch((error) => {
             console.log('error: ', error);
         });
-});
-
-//POST request to add a new comment
-router.post('/save', (req, res) => {
-    console.log('Body: ', req.body);
-    const data = req.body;
-
-    const newComments = new Comments(data);
-
-    //save the data
-    newComments.save((error) => {
-        if (error) {
-            res.status(500).json({ msg: 'Sorry, internal server error' });
-            return;
-        }
-        // Comments
-        return res.json({
-            msg: 'Your data has been saved!!!!!!',
-        });
-    });
-});
-
-//GET request for all comments
-router.get('/name', (req, res) => {
-    const data = {
-        username: 'peterson',
-        age: 5,
-    };
-    res.json(data);
 });
 
 export default router;
